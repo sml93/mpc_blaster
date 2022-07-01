073758ff1fee860e40e67dec5ceea0ce4eb6c769
SML <shawnmlee93@gmail.com>
2022-07-01 12:03:11 +0800

    adding get pose scripts

--- src/scripts/getPose_scripts/getPose_ext.py
+++ src/scripts/getPose_scripts/getPose_ext.py
@@ -17,22 +17,22 @@ def main():
   rospy.init_node('getPose', anonymous=True)
   get = pose()
   rospy.Subscriber('mavros/local_position/pose', PoseStamped, get.uav_pose_cb)
-  # px = uav_pose.pose.position.x
-  # py = uav_pose.pose.position.y
+  px = uav_pose.pose.position.x
+  py = uav_pose.pose.position.y
   pz = uav_pose.pose.position.z
-  # ox = uav_pose.pose.orientation.x
-  # oy = uav_pose.pose.orientation.y
-  # oz = uav_pose.pose.orientation.z
-  print(pz)
-  # pose_array = np.array((px, py, pz, ox, oy, oz))     # Slow will have a substantial lag behind the actual data. 
-  # print(pose_array)
+  ox = uav_pose.pose.orientation.x
+  oy = uav_pose.pose.orientation.y
+  oz = uav_pose.pose.orientation.z
+  # print(pz)
+  pose_array = np.array((px, py, pz, ox, oy, oz))     # Slow will have a substantial lag behind the actual data. 
+  print(pose_array)
 
 
 if __name__=='__main__':
   try:
     while not rospy.is_shutdown():
       main()
-    rospy.Rate(1000).sleep()
+    rospy.Rate(100).sleep()
   except rospy.ROSInterruptException:
     pass
   except KeyboardInterrupt:
