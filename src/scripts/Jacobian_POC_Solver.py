677d7783eb9ad1d742ad3c6e84108f68976634a1
Chien Jer Luen <jerluen0@gmail.com>
2022-08-01 16:01:01 +0800

    worked on finite differences but does not seem to pass checks.

--- src/scripts/Jacobian_POC_Solver.py
+++ src/scripts/Jacobian_POC_Solver.py
@@ -10,19 +10,18 @@ import numpy as np
 import scipy
 from scipy import linalg
 import time
-from htm import compute_T_b_s2, computer_T_w_b
+from htm import compute_T_b_s2, compute_T_w_b
 
 from matplotlib import pyplot as plt
 
-class Jacobian_POC_Solver: 
 
+class Jacobian_POC_Solver:
 
-    def __init__(self, streamVelocity, M_c, Ts): 
-
+    def __init__(self, streamVelocity, M_c, Ts):
         """
 
             How to use this solver: 
-            
+
                 1. Based on UAV position, set initial conditions.
                     a. We need 4 main states. (x, y, z position of the drone and initial vel_x,y,z of the fluids out of the UAV.
                 2. To solve for Jacobian, need to perform finite differences on: 
@@ -32,15 +31,26 @@ class Jacobian_POC_Solver:
                 3. Then, the Jacobians can be appended to the controller model to better predict 
                     a. POC 
                     b. Reaction forces acting on the UAV. 
-        
-        """ 
 
+        """
+
+        self._eps = 1e-6
         self._streamVelocity = np.array([0, 0, streamVelocity])
         self._M_c = M_c
         self._Ts = Ts
         self._initConditions = np.zeros(6)
 
-    def _createIntegrator(self): 
+        self._euler_angles = np.zeros(3)
+        self._motor_angles = np.zeros(2)
+        self._positions = np.zeros(3)
+
+        self._POC = np.zeros(3)
+
+        self._J_pos = np.zeros((3, 3))
+        self._J_eul = np.zeros((3, 3))
+        self._J_mot = np.zeros((3, 2))
+
+    def _createIntegrator(self):
 
         # Initialise states
 
@@ -60,10 +70,10 @@ class Jacobian_POC_Solver:
 
         v_dot = - self._M_c @ self._v + self._g
 
-        # Create model 
+        # Create model
 
         model.name = model_name
-        model.x = states 
+        model.x = states
         model.xdot = statesdot
         model.f_expl_expr = vertcat(self._v, v_dot)
         model.f_impl_expr = statesdot - vertcat(self._v, v_dot)
@@ -81,94 +91,103 @@ class Jacobian_POC_Solver:
         # sim.solver_options.sens_hess = True
         # sim.solver_options.sens_adj = True
 
-        # Create integrator. 
+        # Create integrator.
 
         self.integrator = AcadosSimSolver(sim)
 
-    def _solveForwardIntegration(self, initConditions, T): 
+    def _solveForwardIntegration(self, initConditions, T):
 
         # Set initial conditions
         self.integrator.set('T', T)
         self.integrator.set('x', initConditions)
-        # Integrate 
+        # Integrate
         self.integrator.solve()
         sol = self.integrator.get('x')
         # Return states
         return sol
 
-
-    def _solveRootFindingProblem(self, initialGuess, function): 
-
+    def _solveRootFindingProblem(self, initialGuess, function, initConditions):
         """Initial guess is the amount of time to hit the ground.
         Function is the term that needs to be solved for zero."""
 
-        # Use root-finding algorithm 
+        # Use root-finding algorithm
         #
         # Get time required for fluid to hit the ground
 
-        # Takes about 0.005 - 0.09 seconds depending on how small the time step is with the old step. 
+        # Takes about 0.005 - 0.09 seconds depending on how small the time step is with the old step.
         # New method takes about 0.0007 seconds.
 
         T_N = initialGuess
-        error = 100 # For initialisation sake
+        error = 100  # For initialisation sake
         steps_taken = 0
 
-        while np.abs(error) > 1e-3: 
+        while np.abs(error) > 1e-3:
 
-            T_Nplus1 = self._rootFindingStep(T_N, function)
-            if T_Nplus1 < 0: 
+            T_Nplus1 = self._rootFindingStep(T_N, function, initConditions)
+            if T_Nplus1 < 0:
                 T_Nplus1 = 0
-            error = function(T_Nplus1)
+            error = function(T_Nplus1, initConditions)
             T_N = T_Nplus1
             steps_taken += 1
 
-        return T_Nplus1 
+        return T_Nplus1
 
-    def _rootFindingStep(self, T_N, function):
+    def _rootFindingStep(self, T_N, function, initConditions):
 
         # Just to solve 1 step.
 
-        f = function(T_N)
+        f = function(T_N, initConditions)
         delta_T_N = 1e-1
         T_N_plus = T_N + delta_T_N
-        f_prime = (function(T_N_plus) - f)/delta_T_N
+        f_prime = (function(T_N_plus, initConditions) - f) / delta_T_N
 
-        T_Nplus1 = T_N - (f / f_prime) 
+        T_Nplus1 = T_N - (f / f_prime)
 
         return T_Nplus1
 
-    def solveJacobians(self, R_Nozzle): 
+    def setInitConditions(self, euler_angles, motor_angles, position):
 
-        """R_Nozzle: Numpy 3x3 array. Need to get the R_Nozzle to get initial velocity in x-y-z direction"""
-
-        # Use finite-differences to get dot p_{POC}. 
-
-
-        pass
-
-    def setInitConditions(self, initConditions):
+        self._euler_angles = euler_angles
+        self._motor_angles = motor_angles
+        self._positions = position
+        T = compute_T_w_b(euler_angles[0], euler_angles[1], euler_angles[
+                          2], position) @ compute_T_b_s2(motor_angles[0], motor_angles[1])
+        p = T[0:3, 3]
+        R = T[0:3, 0:3]
+        initConditions = np.hstack((p, R@self._streamVelocity))
 
         self._initConditions = initConditions
 
-    def getJacobians(self): 
+    def setInitConditions_Plus(self, euler_angles, motor_angles, position):
 
-        # This is the main Jacobians getter function. 
-        
+        T = compute_T_w_b(euler_angles[0], euler_angles[1], euler_angles[
+                          2], position) @ compute_T_b_s2(motor_angles[0], motor_angles[1])
+        p = T[0:3, 3]
+        R = T[0:3, 0:3]
+        initConditions = np.hstack((p, R@self._streamVelocity))
+
+        return initConditions
+
+    def getJacobians(self):
+
+        # This is the main Jacobians getter function.
+
         pass
 
-    def getInitConditions(self): 
+    def getInitConditions(self):
 
-        # Based on the pose of the drone and orientation of the nozzle, we can get the initial states of the system.
+        # Based on the pose of the drone and orientation of the nozzle, we can
+        # get the initial states of the system.
 
         initConditions = None
 
         return initConditions
 
-    def _simulateBlastPlot(self, T): 
+    def _simulateBlastPlot(self, T):
 
         N = int(T / self._Ts)
 
-        data = np.zeros((N+1, 6))
+        data = np.zeros((N + 1, 6))
 
         # run getInitConditions
 
@@ -177,20 +196,19 @@ class Jacobian_POC_Solver:
 
         data[0, :] = x
 
-        for i in range(N): 
+        for i in range(N):
 
             x = self._solveForwardIntegration(x, self._Ts)
-            data[i+1, :] = x
+            data[i + 1, :] = x
 
         print(T)
-        
+
         self.integrator.set('x', self._initConditions)
-        self.integrator.set('T', 0.0416623212)
-        # Integrate 
+        self.integrator.set('T', 0.2421937)
+        # Integrate
         self.integrator.solve()
         sol = self.integrator.get('x')
         print(sol)
-
 
         ax = plt.axes(projection='3d')
         ax.set_xlabel('X')
@@ -200,35 +218,80 @@ class Jacobian_POC_Solver:
 
         plt.show()
 
-    def _function(self, T_N):
-
+    def _function(self, T_N, initConditions):
         """This function assumes that we are shooting on the ground. 
         If we are shooting onto the side of a wall, try: 
         return || self._simulateBlast(T_N)[0] + self._simulateBlast(T_N)[1] ||"""
 
-        return self._solveForwardIntegration(self._initConditions, T_N)[2]
+        return self._solveForwardIntegration(initConditions, T_N)[2]
 
-    def _solveFiniteDifferences(self): 
+    def solveJacobians(self, euler_angles, motor_angles, position):
+        """R_Nozzle: Numpy 3x3 array. Need to get the R_Nozzle to get initial velocity in x-y-z direction"""
 
-        pass
+        # Use finite-differences to get dot p_{POC}.
 
-    def computeInitCondition(self, euler_angles, motor_angles, position): 
+        self.setInitConditions(euler_angles, motor_angles, position)
+        self.integrator.set('x', self._initConditions)
+        self._Ts = self._solveRootFindingProblem(0.1, self._function, self._initConditions)
+        self.integrator.set('T', self._Ts)
+        self.integrator.solve()
+        self._POC = self.integrator.get('x')[0:3]
 
-        T = computer_T_w_b(euler_angles[0], euler_angles[1], euler_angles[2], position) @ compute_T_b_s2(motor_angles[0], motor_angles[1])[0:3, 0:3]
-        p = T[0:3, 3]
-        R = T[0:3, 0:3]
-        self.setInitConditions(p, R@self._streamVelocity)
-        
-        
+        for i in range(3):
 
+            euler_angles[i] += self._eps
+            initConditions = self.setInitConditions_Plus(
+                euler_angles, motor_angles, position)
+            self.integrator.set('x', initConditions)
+            self._Ts = self._solveRootFindingProblem(0.1, self._function, initConditions)
+            self.integrator.set('T', self._Ts)
+            self.integrator.solve()
+            POC_plus = self.integrator.get('x')[0:3]
+            J_i = self._solveFiniteDifferences(POC_plus, self._POC)
+            self._J_eul[:, i] = J_i
+            euler_angles = self._euler_angles
+
+        for i in range(2):
+
+            motor_angles[i] += self._eps
+            initConditions = self.setInitConditions_Plus(
+                euler_angles, motor_angles, position)
+            self.integrator.set('x', initConditions)
+            self._Ts = self._solveRootFindingProblem(0.1, self._function, initConditions)
+            self.integrator.set('T', self._Ts)
+            self.integrator.solve()
+            POC_plus = self.integrator.get('x')[0:3]
+            J_i = self._solveFiniteDifferences(POC_plus, self._POC)
+            self._J_mot[:, i] = J_i
+            motor_angles = self._motor_angles
+
+        for i in range(3):
+
+            position[i] += self._eps
+            initConditions = self.setInitConditions_Plus(
+                euler_angles, motor_angles, position)
+            self.integrator.set('x', initConditions)
+            self._Ts = self._solveRootFindingProblem(0.1, self._function, initConditions)
+            self.integrator.set('T', self._Ts)
+            self.integrator.solve()
+            POC_plus = self.integrator.get('x')[0:3]
+            J_i = self._solveFiniteDifferences(POC_plus, self._POC)
+            self._J_pos[:, i] = J_i
+            position = self._positions
+
+    def _solveFiniteDifferences(self, POC_plus, POC):
+
+        return (POC_plus - POC) / self._eps
 
 
 if __name__ == "__main__":
 
     solver = Jacobian_POC_Solver(20, 1.0, 0.00015)
     solver._createIntegrator()
-    solver.setInitConditions(initConditions)
+    solver.setInitConditions([0, 0, 0], [0, 0], [0, 0, 4])
     t0 = time.time()
-    sol = solver._solveRootFindingProblem(0.001, solver._function)
-    print("Time Elapsed: ",time.time() - t0)
+    solver.solveJacobians([0, 0, 0], [10, 0], [0, 0, 4])
+    print("Time Elapsed: ", time.time() - t0)
+    sol = solver._solveRootFindingProblem(0.001, solver._function, solver._initConditions)
+    
     solver._simulateBlastPlot(sol)
--- src/scripts/htm.py
+++ src/scripts/htm.py
@@ -2,82 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-alpha1 = -np.deg2rad(10)      # pitch of M1
-alpha2 = np.deg2rad(0)       # roll of M2
-theta = np.deg2rad(0)
-
-ybs = np.deg2rad(0)
-gamma_ob = np.deg2rad(0)
-psi_ob = np.deg2rad(0)
-
-lob = 1.0
-lbs = 0.2
-ls1s2 = 0.01    # if this is 0, then the point of pitching is at the S2'
-lsn = 0.12
-ds1s2 = 0.05
-y = 0   # to fill with the position/link in the y axis
-
-''' From o to b: @ b '''
-hob = np.array([[np.cos(psi_ob), np.sin(psi_ob), 0, lob*np.cos(gamma_ob)*np.cos(psi_ob)],
-                [np.sin(psi_ob), np.cos(psi_ob), 0, lob*np.cos(gamma_ob)*np.cos(psi_ob)],
-                [0,              0,              1, lob*np.sin(gamma_ob)*np.cos(psi_ob)],
-                [0,              0,              0, 1]])
-
-''' From b to s: @ s1 '''
-hbs1 = np.array([[ 1,   0,   0,    lbs*np.sin(ybs)],
-                 [ 0,   1,   0,    y              ],
-                 [ 0,   0,   1,    lbs*np.cos(ybs)],
-                 [ 0,   0,   0,    1              ]])
-
-''' From s1 to s2: @ s2'''
-hs1s2 = np.array([[ np.cos(alpha1),  0,  np.sin(alpha1), ls1s2*np.cos(alpha1)],
-                  [ 0,               1,  0,              y                   ],
-                  [ -np.sin(alpha1), 0,  np.cos(alpha1), -ls1s2*np.sin(alpha1)],
-                  [ 0,               0,  0,              1                   ]])
-
-''' From s2 to n: @ n'''
-hs2n = np.array([[ 1,    0,               0,               -ls1s2                ],
-                 [ 0,    np.cos(alpha2),  np.sin(alpha2),  y + lsn*np.sin(alpha2)],
-                 [ 0,    np.sin(alpha2),  np.cos(alpha2),  -lsn*np.cos(alpha2)   ],
-                 [ 0,    0,               0,               1                     ]])
-
-p_i = np.array([[0], [0], [0], [1]])
-
-p_bM = hob @ p_i
-print(p_bM)
-
-# p_s1 = np.dot(hbs1, p_i)
-p_s1M = (hob @ hbs1) @ p_i
-print(p_s1M)
-
-# p_s2 = np.dot(hs1s2, p_i)
-p_s2M = (hob @ hbs1 @ hs1s2) @ p_i
-print(p_s2M)
-
-# p_n = np.dot(hs2n, p_i)
-p_nM = (hob @ hbs1 @ hs1s2 @ hs2n) @ p_i
-print(hbs1 @ hs1s2 @ hs2n)
-print(p_nM)
-
-xcoord = [p_s1M[0][0], p_s2M[0][0], p_nM[0][0]]
-ycoord = [p_s1M[1][0], p_s2M[1][0], p_nM[1][0]]
-zcoord = [p_s1M[2][0], p_s2M[2][0], p_nM[2][0]]
-
-# print(p_nM)
-
-''' Plotting positions '''
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.scatter3D(xcoord, ycoord, zcoord, c='k')
-ax.plot3D(xcoord, ycoord, zcoord)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-# ax.set_xlim([1.0, 1.1])
-# ax.set_ylim([1.0, 1.05])
-# ax.set_zlim([0.16, 0.26])
-plt.show()
+from scipy.spatial.transform import Rotation as R
 
 def compute_T_b_s2(alpha1, alpha2):
 
@@ -85,7 +10,7 @@ def compute_T_b_s2(alpha1, alpha2):
     hbs1 = np.array([[ 1,   0,   0,   0.01672               ],
                     [ 0,   1,   0,    0                     ],
                     [ 0,   0,   1,    -0.22937              ],
-                    [ 0,   0,   0,    1                    ]])
+                    [ 0,   0,   0,    1                    ]]) # ANGLES SEEM TO BE HERE INSTEAD. 
 
     ''' From s1 to s2: @ s2'''
     
@@ -95,14 +20,18 @@ def compute_T_b_s2(alpha1, alpha2):
                     [ 0,               0,  0,              1                 ]])
 
     ''' From s2 to n: @ n'''
-    hs2n = np.array([[ 1,    0,               0,              -0.05322              ],
-                    [ 0,    np.cos(alpha2),  np.sin(alpha2),  0                     ],
-                    [ 0,    np.sin(alpha2),  np.cos(alpha2),  -0.15946              ],
+    hs2n = np.array([[ 1,    0,               0,              -0.05322               ],
+                    [ 0,    np.cos(alpha2),  np.sin(alpha2),  0                      ],
+                    [ 0,    -np.sin(alpha2),  np.cos(alpha2),  -0.15946              ],
                     [ 0,    0,               0,               1                     ]])
 
     return hbs1@hs1s2@hs2n
 
+def compute_T_w_b(phi, theta, psi, position): 
 
-def computer_T_w_b(phi, theta, psi, position): 
+    T = np.eye(4)
+    Rot = R.from_euler('zyx', [psi, theta, phi])
+    T[0:3, 0:3] = Rot.as_matrix()
+    T[0:3, 3] = position
 
-    pass 
+    return T
