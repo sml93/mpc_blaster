93a2d622109f5a2247c00b1d499c1534fc87ef14
sml93 <shawnmlee93@gmail.com>
2022-10-19 01:26:57 +0800

    Re-worked scripts for closed loop gazebo sim.

--- src/scripts/mavros_blaster_realtimeV2.py
+++ src/scripts/mavros_blaster_realtimeV2.py
@@ -25,7 +25,7 @@ class blasterController:
 
   def init_nodes(self):
     rospy.init_node('blaster_controller')
-    self.rate = rospy.Rate(10)
+    self.rate = rospy.Rate(20)
     self.init_pubsubs()
     self.run_node()
     self.rate.sleep()
@@ -34,7 +34,7 @@ class blasterController:
     avg = thrusterCoefficient*np.mean([t1,t2,t3,t4])/9.81
     T_sp = (0.0014*np.power(avg,3)) - (0.0263*np.power(avg,2)) + (0.2464*avg) -0.0286
     if (T_sp >= 1.0):
-      T_sp = 0.77
+      T_sp = 0.8
     print('Thrust: ', round(T_sp,3))
     return T_sp
 
@@ -71,12 +71,17 @@ class blasterController:
     Q_t = 10*Q
     R = np.zeros((6, 6))
     np.fill_diagonal(R, [5e-2, 5e-2, 5e-2, 5e-2, 1e-5, 1e-5])
-    statesBound = np.array([[-5, -5, -1.0, -0.174532925, -0.174532925, -0.349066, -1.0, -1.0, -0.5, -0.0872665, -0.0872665, -0.0872665, -0.174532925, -0.523599, -1.5, -1.5, 0.0],
-                            [5, 5, 10, 0.174532925, 0.174532925, 0.349066, 1.0, 1.0, 0.5, 0.0872665, 0.0872665, 0.0872665, 1.22173, 0.523599, 1.5, 1.5, 2.5]])
+    statesBound = np.array([[-5, -5, -1.0, -0.174532925, -0.174532925, -0.349066, -1.0, -1.0, -0.5, -0.0872665, -0.0872665, -0.0872665, -0.174532925, -0.523599, -5, -5, -1.0],
+                            [5, 5, 10, 0.174532925, 0.174532925, 0.349066, 1.0, 1.0, 0.5, 0.0872665, 0.0872665, 0.0872665, 1.22173, 0.523599, 5, 5, 10.0]])
     controlBound = np.array([[0, 0, 0, 0, -0.0872665, -0.0872665], [65, 65, 65, 65, 0.0872665, 0.0872665]])
     b = blasterModel(self.mass, self.J, self.l_x, self.l_y, self.N, Tf, self.yaw_coeff, Q, R, Q_t, self.blastThruster, statesBound, controlBound)
     b.generateModel()
     self.integrator, self.ocp_solver = b.generateController()
+
+    solver = Jacobian_POC_Solver(150, 1, 0.000015)
+    solver.initialise()
+    self.J_mot, self.J_eul, self.J_pos = solver.getJacobians()
+
 
   def callback(self, msg):
     self.blaster_states[0] = round(msg.pose.pose.position.x, 3)
@@ -103,11 +108,17 @@ class blasterController:
     self.ocp_solver.cost_set(0, 'yref', self.yref)
 
     for k in range(self.N): 
+      params = np.vstack((np.reshape(self.J_mot, (self.J_mot.size, 1), order='F'), np.reshape(self.J_eul, (self.J_eul.size, 1), order='F'), np.reshape(self.J_pos, (self.J_pos.size, 1), order='F'), self.blastThruster))
+      self.ocp_solver.set(k, 'p', params)
+
       if k+1 == self.N: 
         self.ocp_solver.cost_set(k+1, 'yref', self.yref[0:self.nx])
       else: 
         self.ocp_solver.cost_set(k+1, 'yref', self.yref)
+
     self.ocp_solver.solve()
+    params = np.vstack((np.reshape(self.J_mot, (self.J_mot.size, 1), order='F'), np.reshape(self.J_eul, (self.J_eul.size, 1), order='F'), np.reshape(self.J_pos, (self.J_pos.size, 1), order='F'), self.blastThruster))
+    # self.integrator.set('p', params)
     print(f"ocp_solver.get_cost(): {self.ocp_solver.get_cost()}")
     
     self.attitude_target_msg.type_mask = 7
@@ -134,7 +145,7 @@ class blasterController:
 
 if __name__ == '__main__':
   # Drone variables
-  mass = 9.0
+  mass = 8.0
   J = np.eye(3)
   J[0, 0] = 0.50781
   J[1, 1] = 0.50781   #0.47314
@@ -144,106 +155,13 @@ if __name__ == '__main__':
   yaw_coefficient = 0.03
   # blastThruster = 2.2*9.81
   blastThruster = 0.0
-  thrusterCoefficient = 1.0
+  thrusterCoefficient = 3.7
   blaster_states = np.zeros((17))
-  yref = np.array([0.0, 0.0, 2.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.2, 0.0, 0, 0, 0, 0, 0.0, 0, 0])
-  blaster_params = {"yref": yref, "mass": mass,"J": J, "l_x": l_x, "l_y": l_y, "N": 100, "yaw_coefficient": yaw_coefficient, "blastThruster": thrusterCoefficient}
+  yref = np.array([0.0, 0.0, 2.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.00, 0.0, 0, 0, 0, 0, 0.0, 0, 0])
+  blaster_params = {"yref": yref, "mass": mass,"J": J, "l_x": l_x, "l_y": l_y, "N": 200, "yaw_coefficient": yaw_coefficient, "blastThruster": thrusterCoefficient}
   try: 
     blasterController(blaster_params)
     rospy.spin()
   except rospy.ROSInterruptException:
     pass
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  # # Drone parameters
-  # N = 30
-  # Tf = 1.0
-  # Q = np.zeros((17, 17))
-  # np.fill_diagonal(Q, [1e2, 1e2, 1e2, 1e2, 1e2, 1e2, 0.5e1, 0.5e1, 0.5e1, 1e1, 1e1, 1e1, 1e-2, 1e-2, 0.01e2, 0.01e2, 0.01e2]) # position, euler, velocity, angular velocity, swivel angles, POC.
-  # Q_t = 10*Q
-  # R = np.zeros((6, 6))
-  # np.fill_diagonal(R, [5e-2, 5e-2, 5e-2, 5e-2, 1e1, 1e1])
-  # statesBound = np.array([[-1.5, -1.5, 0, -0.174532925, -0.174532925, -0.349066, -0.5, -0.5, -0.5, -0.0872665, -0.0872665, -0.0872665, -0.174532925, -0.523599, -1.5, -1.5, -2.5],
-  #                         [1.5, 1.5, 5.0, 0.174532925, 0.174532925, 0.349066, 0.4, 0.5, 1.0, 0.0872665, 0.0872665, 0.0872665, 1.22173, 0.523599, 1.5, 1.5, 2.5]])
-  # controlBound = np.array([[0, 0, 0, 0, -0.0872665, -0.0872665], [65, 65, 65, 65, 0.0872665, 0.0872665]])
-  # b = blasterModel(mass, J, l_x, l_y, N, Tf, yaw_coefficient, Q, R, Q_t, blastThruster, statesBound, controlBound)
-  # b.generateModel()
-  # integrator, ocp_solver = b.generateController()
-
-  # # Simulation parameters
-  # nx = 17 
-  # nu = 6
-  # Nsim = 1000
-  # simX = np.ndarray((Nsim+1, nx))
-  # simU = np.ndarray((Nsim, nu))
-
-  # x0 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-  # yref = np.array([0.5, 1.0, 3.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-  # t = np.linspace(0, Tf/N*Nsim, Nsim+1)
-  # simX[0, :] = x0
-
-  # xcurrent = x0 
-
-  # for i in range(Nsim): 
-
-  #   t0 = time.time()
-
-  #   ocp_solver.set(0, "lbx", xcurrent)
-  #   ocp_solver.set(0, "ubx", xcurrent)
-
-  #   ocp_solver.cost_set(0, 'yref', yref)
-
-  #   for k in range(N):
-        
-  #     if k+1 == N: 
-
-  #       status = ocp_solver.cost_set(k+1, 'yref', yref[0:nx])
-
-  #     else: 
-  #       status = ocp_solver.cost_set(k+1, 'yref', yref)
-
-  #   status = ocp_solver.solve()
-  #   print(xcurrent)
-  #   print(ocp_solver.get_cost())
-  #   print(ocp_solver.get(0, "u")[0:4])      # collective thrust
-  #   print(ocp_solver.get(0, "x")[3:6])      # euler angles
-
-  #   simU[i,:] = ocp_solver.get(0, "u")
-
-  #   # u = np.array([])
-
-  #   # simulate system
-  #   integrator.set("x", xcurrent)
-  #   integrator.set("u", simU[i,:])
-  #   # integrator.set("u", np.array([22.0725, 22.0725, 22.0725, 22.0725, 0, 0]))
-
-  #   status = integrator.solve()
-  #   if status != 0:
-  #       raise Exception('acados integrator returned status {}. Exiting.'.format(status))
-
-  #   # update state
-  #   xcurrent = integrator.get("x")
-  #   simX[i+1,:] = xcurrent
-
-  #   print(f"Time per step: {time.time() - t0}")
-
-  # # plt.plot(t, simX[:, 6:9])
-  # plt.plot(t, simX[:, 0:3])
-  # # plt.plot(t[0:Nsim], simU[:, 0:4])
-  # plt.show()
